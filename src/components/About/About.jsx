927d126f62f19a3a54e4c7bdeb842b44bd0e030b
ImoDavid <emperorjagz@gmail.com>
2024-07-30 01:22:10 +0100

    feat: experience / projects updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,51 +1,52 @@
-import React from "react";
-import { Container, Stack, Typography, Box, styled } from "@mui/material";
-import { BsGit } from "react-icons/bs";
-import { FaBootstrap, FaReact, FaSass ,FaNodeJs } from "react-icons/fa";
+import React from 'react';
+import { Container, Stack, Typography, Box, styled } from '@mui/material';
+import { BsGit } from 'react-icons/bs';
+import { FaBootstrap, FaReact, FaSass, FaNodeJs } from 'react-icons/fa';
+import { SiNestjs, SiTailwindcss } from 'react-icons/si';
 import {
   BiLogoMongodb,
   BiLogoJavascript,
   BiLogoTypescript,
-} from "react-icons/bi";
-import { FiFigma } from "react-icons/fi";
-import { AiOutlineAntDesign } from "react-icons/ai";
-import { colors } from "../../styles/globals";
-import { Heading } from "../typography/";
-import { motion } from "framer-motion";
+} from 'react-icons/bi';
+import { FiFigma } from 'react-icons/fi';
+import { SiPhp } from 'react-icons/si';
+import { colors } from '../../styles/globals';
+import { Heading } from '../typography/';
+import { motion } from 'framer-motion';
 
 const StyledStack = styled(Box)({
-  display: "flex",
-  alignItems: "center",
+  display: 'flex',
+  alignItems: 'center',
   color: colors.ORANGE,
-  textTransform: "capitalize",
-  fontSize: ["1rem", "0.4rem"],
-  marginBottom: ["0.2rem", "0.2rem"],
+  textTransform: 'capitalize',
+  fontSize: ['1rem', '0.4rem'],
+  marginBottom: ['0.2rem', '0.2rem'],
 });
 
 const StyledAbouttext = styled(Typography)({
   color: colors.OFF_WHITE,
-  fontWeight: "400",
-  fontSize: "1rem",
-  "@media screen and (max-width: 1200px)": {
-    fontWeight: "300",
-    fontSize: "0.9rem",
+  fontWeight: '400',
+  fontSize: '1rem',
+  '@media screen and (max-width: 1200px)': {
+    fontWeight: '300',
+    fontSize: '0.9rem',
   },
 });
 
 const boxItemsone = [
-  { id: "0.1", icon: <FaBootstrap />, stack: "Bootstrap" },
-  { id: "0.2", icon: <FaReact />, stack: "react.js" },
-  { id: "0.3", icon: <BsGit />, stack: "git" },
-  { id: "0.4", icon: <BiLogoMongodb />, stack: "Mongo db" },
-  { id: "0.5", icon: <FiFigma />, stack: "Figma" },
+  { id: '0.2', icon: <FaReact />, stack: 'react.js' },
+  { id: '0.3', icon: <BsGit />, stack: 'git' },
+  { id: '0.4', icon: <BiLogoMongodb />, stack: 'Mongo db' },
+  { id: '0.5', icon: <FiFigma />, stack: 'Figma' },
+  { id: '0.6', icon: <SiNestjs />, stack: 'Nest.js' },
 ];
 
 const boxItemstwo = [
-  { id: "0.1", icon: <BiLogoJavascript />, stack: "Javascript (Es6+)" },
-  { id: "0.2", icon: <AiOutlineAntDesign />, stack: "ant design" },
-  { id: "0.4", icon: <FaSass />, stack: "sass" },
-  { id: "0.3", icon: <BiLogoTypescript />, stack: "Typescript" },
- { id: "0.4", icon: <FaNodeJs />, stack: "Node.js" },
+  { id: '0.1', icon: <BiLogoJavascript />, stack: 'Javascript (Es6+)' },
+  { id: '0.7', icon: <FaSass />, stack: 'sass' },
+  { id: '0.3', icon: <BiLogoTypescript />, stack: 'Typescript' },
+  { id: '0.4', icon: <FaNodeJs />, stack: 'Node.js' },
+  { id: '0.6', icon: <SiTailwindcss />, stack: 'Tailwind css' },
 ];
 
 const About = () => {
@@ -57,49 +58,45 @@ const About = () => {
         component={motion.div}
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ type: "spring", stiffness: 100, delay: 0.3 }}
+        transition={{ type: 'spring', stiffness: 100, delay: 0.3 }}
       >
         <Box
-          minHeight={"100%"}
-          paddingX={["1rem", "5rem"]}
-          marginTop={["0.8rem", "1rem"]}
-          marginBottom={["1rem", "2rem"]}
+          minHeight={'100%'}
+          paddingX={['1rem', '5rem']}
+          marginTop={['0.8rem', '1rem']}
+          marginBottom={['1rem', '2rem']}
         >
           <Box>
-            <Heading number={"01."} title={"About Me"} />
+            <Heading number={'01.'} title={'About Me'} />
           </Box>
-          <Stack direction={["column", "row"]}>
-            <Box width={["100%", "65%"]} paddingY={"1.5rem"}>
+          <Stack direction={['column', 'row']}>
+            <Box width={['100%', '65%']} paddingY={'1.5rem'}>
               <StyledAbouttext>
-                I'm a Javascript developer with over three years experience and a
-                proven track record of translating complex requirements into
-                elegant, responsive websites by weaving creativity and
+                I'm a fullstack web developer with over four years of experience
+                and a proven track record of translating complex requirements
+                into elegant, responsive websites by weaving creativity and
                 functionality together to craft compelling web interfaces that
-                resonate with clients ideas.
+                resonate with clients' ideas. Additionally, I develop robust,
+                scalable APIs, manage databases efficiently, and ensure seamless
+                integration between frontend and backend systems to deliver
+                cohesive and high-performing web applications.
               </StyledAbouttext>
 
-              <StyledAbouttext display={"block"} marginY={"1rem"}>
-                Currently the Lead Developer volunteering for an NGO{" "}
-                <span style={{ color: "orange" }}>Nutrilife </span> where i've
-                embraced the challenge of spearheading our NGO's digital
-                transformation journey by leveraging the latest web technologies
-                and best practices.
-              </StyledAbouttext>
-              <Box marginTop={"2rem"}>
+              <Box marginTop={'2rem'}>
                 <Typography
-                  variant={"p"}
-                  fontSize={["1rem", "0.9rem"]}
+                  variant={'p'}
+                  fontSize={['1rem', '0.9rem']}
                   color={colors.OFF_WHITE}
                   fontWeight={500}
                 >
                   Here are a few Technologies I work with:
                 </Typography>
-                <Stack direction={"row"}>
+                <Stack direction={'row'}>
                   <Box marginY={3} mr={[6, 20]}>
                     {boxItemsone.map((ele) => (
                       <StyledStack key={ele.id}>
-                        <Typography fontSize={"0.9rem"}>
-                          {" "}
+                        <Typography fontSize={'0.9rem'}>
+                          {' '}
                           <span>{ele.icon}</span> {ele.stack}
                         </Typography>
                       </StyledStack>
@@ -108,8 +105,8 @@ const About = () => {
                   <Box marginY={3}>
                     {boxItemstwo.map((ele) => (
                       <StyledStack key={ele.id}>
-                        <Typography fontSize={"0.9rem"}>
-                          {" "}
+                        <Typography fontSize={'0.9rem'}>
+                          {' '}
                           <span>{ele.icon}</span> {ele.stack}
                         </Typography>
                       </StyledStack>
@@ -119,32 +116,32 @@ const About = () => {
               </Box>
             </Box>
             <Box
-              width={["100%", "35%"]}
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
+              width={['100%', '35%']}
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
             >
               <Box
                 sx={{
-                  borderRight: "2px solid orange",
-                  borderBottom: "2px solid orange",
-                  padding: "10px",
+                  borderRight: '2px solid orange',
+                  borderBottom: '2px solid orange',
+                  padding: '10px',
                 }}
               >
                 <Box
-                  component={"img"}
+                  component={'img'}
                   src={
-                    "https://res.cloudinary.com/dfljnnxln/image/upload/v1695961717/20230929_045456_aefsuk_-_Profile_Picture_xia0tv.png"
+                    'https://res.cloudinary.com/dfljnnxln/image/upload/v1695961717/20230929_045456_aefsuk_-_Profile_Picture_xia0tv.png'
                   }
                   height={[200, 250]}
                   width={[200, 250]}
-                  display={"block"}
+                  display={'block'}
                   sx={{
-                    objectFit: "contain",
-                    borderRadius: "10px",
-                    filter: "sepia(60%)",
-                    "&:hover": {
-                      filter: "sepia(0%)",
+                    objectFit: 'contain',
+                    borderRadius: '10px',
+                    filter: 'sepia(60%)',
+                    '&:hover': {
+                      filter: 'sepia(0%)',
                     },
                   }}
                 />
@@ -159,21 +156,4 @@ const About = () => {
 
 export default About;
 
-
 //Experienced Fullstack Developer with a proven track record of over 3 years, adept at crafting scalable, maintainable, and efficient web applications. Proficient in both front-end and back-end technologies, with a strong emphasis on delivering high-quality products that meet client needs and exceed expectations. Skilled in designing robust architectures, implementing cutting-edge features, and optimizing performance for optimal user experience. Committed to staying updated with the latest industry trends and continuously refining skills to tackle new challenges effectively.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,57 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Stack, Typography, Box, styled } from "@mui/material";
-import { colors } from "../../styles/globals";
-import { BiFolder } from "react-icons/bi";
-import { FiGithub, FiExternalLink } from "react-icons/fi";
-import { Heading } from "../typography";
-import { Project } from "../../commons/project";
-import { motion } from "framer-motion";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Stack, Typography, Box, styled } from '@mui/material';
+import { colors } from '../../styles/globals';
+import { BiFolder } from 'react-icons/bi';
+import { FiGithub, FiExternalLink } from 'react-icons/fi';
+import { Heading } from '../typography';
+import { Project } from '../../commons/project';
+import { motion } from 'framer-motion';
 
 const StyledLinkBox = styled(Box)({
   color: colors.FOOTER_TEXT,
-  marginRight: "1rem",
-  "& : hover": {
+  marginRight: '1rem',
+  '& : hover': {
     color: colors.ORANGE,
   },
 });
 
 const OtherProjects = [
   {
-    index: "1",
-    title: "shoplist Organizer",
-    text: "A web application to organize shopping or To-Do list with features like Add, Delete and sharing the list.",
-    Glink: "https://shoplistorganiser.netlify.app/",
-    stack: "React.Js React-Bootstrap ReduxToolkit Css Git Netlify.",
+    index: '6',
+    title: 'wearFina',
+    text: 'An E-Commerce web application which possesses user authetication, a cart and checkout system.',
+    Glink: 'https://wearfina.netlify.app/',
+    stack:
+      'React.Js, Styled Components, Redux Toolkit, REST API, SwiperJs, Git, Netlify.',
   },
   {
-    index: "2",
-    title: "easyBank",
-    Glink: "https://clone-easy-bank.netlify.app/",
-    text: "A  single one page website for a digital bank aimed at providing digital financial solutions.",
-    stack: "Html Bootstrap5 Javascript Sass Git Netlify.",
+    index: '2',
+    title: 'easyBank',
+    Glink: 'https://clone-easy-bank.netlify.app/',
+    text: 'A  single one page website for a digital bank aimed at providing digital financial solutions.',
+    stack: 'Html Bootstrap5 Javascript Sass Git Netlify.',
   },
   {
-    index: "3",
-    title: "pizzaHut",
-    text: "A web application that allows visitors/ customers to explore the menu and gallery of a restuarant.",
-    Glink: "https://imodavid.github.io/pizzahut-landing/",
-    stack: "Html Css Javascript SwiperJs Git.",
+    index: '3',
+    title: 'sunnySide',
+    Glink: 'https://imodavid.github.io/sunny-side-agency-landing-page/',
+    text: 'A single pager landing page for a creative agency. ',
+    stack: 'Html, Css, Bootstrap 5, Javascript, Scroll trigger, Git. ',
   },
   {
-    index: "4",
-    title: "zoeyTote",
-    text: "A single landing page for an E-commerce and female fashion suite.",
-    Glink: "https://imodavid.github.io/zoey-tote/",
-    stack: "Html Css Bootstrap5 Git.",
-  },
-  {
-    index: "5",
-    title: "fylo",
-    text: "A landing page for Fylo which helps users safe-keep important documents.",
-    Glink:
-      "https://imodavid.github.io/fylo-landing-page-with-two-column-layout/",
-    stack: "Html Css Git.",
+    index: '1',
+    title: 'shoplist Organizer',
+    text: 'A web application to organize shopping or To-Do list with features like Add, Delete and sharing the list.',
+    Glink: 'https://shoplistorganiser.netlify.app/',
+    stack: 'React.Js React-Bootstrap ReduxToolkit Css Git Netlify.',
   },
 ];
 
@@ -64,83 +57,84 @@ const Projects = () => {
         component={motion.div}
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
-        transition={{ type: "spring", stiffness: 100, delay: 0.3 }}
+        transition={{ type: 'spring', stiffness: 100, delay: 0.3 }}
       >
         <Box
-          minHeight={"100%"}
-          paddingX={["1rem", "5rem"]}
-          marginTop={["13.5rem", "10rem"]}
-          marginBottom={["1rem", "2rem"]}
+          minHeight={'100%'}
+          paddingX={['1rem', '5rem']}
+          marginTop={['13.5rem', '10rem']}
+          marginBottom={['1rem', '2rem']}
         >
           <Box>
-            <Heading number={"03."} title={"things i've built"} />
+            <Heading number={'03.'} title={"things i've built"} />
           </Box>
-          <Box color={colors.OFF_WHITE} paddingY={"1.5rem"} textAlign={"left"}>
-            <Typography fontSize={"0.9rem"} fontWeight={"300"}>
+          <Box color={colors.OFF_WHITE} paddingY={'1.5rem'} textAlign={'left'}>
+            <Typography fontSize={'0.9rem'} fontWeight={'300'}>
               I've made lots of projects, unfortunately i can't show you most of
               them. 😔 But, you can enjoy my personal playgrounds:
             </Typography>
           </Box>
           <Box>
             <Project
-              title="streetPriest"
+              title="Toast"
               buttonText="Our Concept"
               Align="right"
               justify="end"
-              stack={"undisclosed"}
-              des={
-                "I collabrated to work on the frontend development of this web application that helps showcase the amazing work of putting smiles and empowering street kids to become better part of the society. "
-              }
+              stack="undisclosed"
+              des={`
+    I collaborated on the frontend development of a web application designed for restaurants. A platform trusted by 112,000* restaurants, providing comprehensive technology solutions from POS to payroll, allowing restaurant owners to focus on what they do best.
+  `}
               Glink=""
-              Slink={"https://www.streetpriests.org/"}
-              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1695966762/Screenshot_from_2023-09-29_06-51-52_kwmjcj.png"
+              Slink="https://pos.toasttab.com/"
+              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1722295592/Screenshot_from_2024-07-30_00-23-54_zeizma.png"
             />
+
             <Project
               orderBox="2"
               orderPic="1"
-              marginLeft={"3rem"}
+              marginLeft={'3rem'}
               marginRight="0"
-              stack={
-                "React.Js, Styled Components, Redux Toolkit, REST API, SwiperJs, Git, Netlify."
-              }
-              title="wearFina"
+              stack="React.Js, tailwind css, Framer motion,Redux Toolkit, REST API , Git,"
+              title="TECHPAM"
               des={
-                "An E-Commerce web application which possesses user authetication, a cart and checkout system."
+                'Crafted a visually appealing and user-friendly landing page for a software developement agency to showcase its services and portfolio'
               }
               Glink=""
-              Slink={"https://wearfina.netlify.app/"}
-              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1695966282/Screenshot_from_2023-09-29_06-44-22_qteh1q.png"
+              Slink={'https://techpem.com/'}
+              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1722296160/Screenshot_from_2024-07-30_00-35-31_r6v9r2.png"
             />
+
             <Project
               title="Interno"
               buttonText="Our Concept"
               Align="right"
               stack={
-                "React.Js, React-bootstrap, Material UI, Css, Git, Netlify. "
+                'React.Js, React-bootstrap, Material UI, Css, Git, Netlify. '
               }
               justify="end"
               des={
-                "This is a stylish and minimalist multipage web application for an internal decoration agency. "
+                'This is a stylish and minimalist multipage web application for an internal decoration agency. '
               }
               Glink=""
-              Slink={"http://interno-decor.netlify.app/"}
+              Slink={'http://interno-decor.netlify.app/'}
               image="https://res.cloudinary.com/dfljnnxln/image/upload/v1695966069/Screenshot_from_2023-09-29_06-40-23_lrhhmo.png"
             />
             <Project
+              title="streetPriest"
+              buttonText="Our Concept"
               orderBox="2"
               orderPic="1"
-              marginLeft={"3rem"}
-              stack={
-                "Html, Css, Bootstrap 5, Javascript, Scroll trigger, Git. "
-              }
+              marginLeft={'3rem'}
               marginRight="0"
-              title="sunnySide"
-              des={" A single pager landing page for a creative agency. "}
-              Glink=""
-              Slink={
-                "https://imodavid.github.io/sunny-side-agency-landing-page/"
+              // Align="right"
+              // justify="end"
+              stack={'undisclosed'}
+              des={
+                'I collabrated to work on the frontend development of this web application that helps showcase the amazing work of putting smiles and empowering street kids to become better part of the society. '
               }
-              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1696071223/Screenshot_from_2023-09-30_11-36-29_huxvxz.png"
+              Glink=""
+              Slink={'https://www.streetpriests.org/'}
+              image="https://res.cloudinary.com/dfljnnxln/image/upload/v1695966762/Screenshot_from_2023-09-29_06-51-52_kwmjcj.png"
             />
           </Box>
         </Box>
@@ -148,52 +142,58 @@ const Projects = () => {
           component={motion.div}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{ type: "spring", stiffness: 100, delay: 0.3 }}
+          transition={{ type: 'spring', stiffness: 100, delay: 0.3 }}
         >
-          <Box textAlign={"center"} color={colors.FOOTER_TEXT} mt={10}>
-            <Typography fontSize={"1.7rem"} fontWeight={400}>
-              Other Noteworthy projects{" "}
+          <Box textAlign={'center'} color={colors.FOOTER_TEXT} mt={10}>
+            <Typography fontSize={'1.7rem'} fontWeight={400}>
+              Other Noteworthy projects{' '}
             </Typography>
           </Box>
 
-          <Stack mt={7} mb={7} direction={"row"} flexWrap={"wrap"} ml={[0, 5]}>
+          <Stack
+            mt={7}
+            mb={7}
+            direction={'row'}
+            justifyContent={['center', 'start']}
+            flexWrap={'wrap'}
+          >
             {OtherProjects.map((ele) => (
               <Box
-                width={"280px"}
-                height={"280px"}
+                width={'280px'}
+                height={'280px'}
                 backgroundColor={colors.MOBILENAV}
                 m={2}
                 padding={3}
                 key={ele.index}
-                borderRadius={"10px"}
+                borderRadius={'10px'}
                 component={motion.div}
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
-                transition={{ type: "spring", stiffness: 100, delay: 0.3 }}
+                transition={{ type: 'spring', stiffness: 100, delay: 0.3 }}
                 whileHover={{
                   scale: 1.02,
                   transition: { duration: 0.3 },
                 }}
-                onClick={() => window.open(ele.Glink, "_blank")}
-                sx={{ cursor: "pointer" }}
+                onClick={() => window.open(ele.Glink, '_blank')}
+                sx={{ cursor: 'pointer' }}
               >
                 <Stack
-                  direction={"row"}
-                  justifyContent={"space-between"}
-                  alignItems={"center"}
+                  direction={'row'}
+                  justifyContent={'space-between'}
+                  alignItems={'center'}
                   my={2}
                 >
                   <Box>
-                    <BiFolder color={colors.ORANGE} size={"2rem"} />
+                    <BiFolder color={colors.ORANGE} size={'2rem'} />
                   </Box>
-                  <Stack direction={"row"}>
+                  <Stack direction={'row'}>
                     <Link
                       to={ele.Elink}
-                      target={"_blank"}
+                      target={'_blank'}
                       color={colors.FOOTER_TEXT}
                     >
                       <StyledLinkBox>
-                        {<FiGithub size={"1.5rem"} />}
+                        {<FiGithub size={'1.5rem'} />}
                       </StyledLinkBox>
                     </Link>
                     <Link
@@ -202,7 +202,7 @@ const Projects = () => {
                       color={colors.FOOTER_TEXT}
                     >
                       <StyledLinkBox>
-                        {<FiExternalLink size={"1.5rem"} />}
+                        {<FiExternalLink size={'1.5rem'} />}
                       </StyledLinkBox>
                     </Link>
                   </Stack>
@@ -218,17 +218,17 @@ const Projects = () => {
                   </Typography>
                   <Typography
                     color={colors.OFF_WHITE}
-                    fontSize={"0.7rem"}
+                    fontSize={'0.7rem'}
                     fontWeight={700}
                   >
                     {ele.text}
                   </Typography>
                   <Typography
                     mt={1.0}
-                    fontSize={"0.8rem"}
+                    fontSize={'0.8rem'}
                     color={colors.OFF_WHITE}
                   >
-                    {" "}
+                    {' '}
                     Stack:{ele.stack}
                   </Typography>
                 </Box>
